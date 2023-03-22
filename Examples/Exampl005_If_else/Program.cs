5c5070fd772658b6f80fd0f7166cbed137736c05
ваше_имя <grigorenko-marko@rambler.ru>
2023-03-22 23:34:44 +0300

    Example005 if else

---
+++ Examples/Exampl005_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Add your user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Yo, this is Masha!");
+}
+else 
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
