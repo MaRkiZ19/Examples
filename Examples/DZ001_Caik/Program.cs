4caf45f46446906ea77b7ee3f0f8bc1b01502140
ваше_имя <grigorenko-marko@rambler.ru>
2023-03-23 17:46:59 +0300

    addet calk

---
+++ Examples/DZ001_Caik/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("numberA = ");
+string numberA = Console.ReadLine();
+double number1 = double.Parse(numberA);
+
+Console.Write("enter action: ");
+string action = Console.ReadLine();
+
+Console.Write("numberB = ");
+string numberB = Console.ReadLine();
+double number2 = double.Parse(numberB);
+
+if (action.ToLower() == "+")
+{
+    Console.WriteLine(number1 + number2);
+}
+else 
+{
+    if (action.ToLower() == "-")
+    {
+        Console.WriteLine(number1 - number2);
+    }
+    else
+    {
+        if (action.ToLower() == "*")
+        {
+            Console.WriteLine(number1 * number2);
+        }
+        else
+        {
+            Console.WriteLine(number1 / number2);
+        }
+    }
+}
+
+
+
+
